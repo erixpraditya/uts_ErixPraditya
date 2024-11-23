8c3f1350383699d9af2b81964211cb21f1bdcfe6
erixpraditya <erixpradityaa@gmail.com>
2024-11-23 09:44:23 +0700

    menambahkan index koneksi navbar

---
+++ index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>UTS IS62</title>
+    <link rel="stylesheet" href="css/bootstrap.css">
+</head>
+<body>
+    <?php
+         include_once('navbar.php');
+    ?>
+
+<div class="container">
+    <div class="row mt-5">
+        <div class="col-6 m-auto">
+            <div class="card">
+        <div class="card-header text-center">
+            <h3>Library Manajer</h3>
+        </div>
+        <div class="card-body">
+                <h4>Selamat Datang</h4>
+                <p>Selamat Datang di Library Manager 
+                    Halo! 👋  
+                    Selamat datang di Library Manager, platform yang dirancang untuk memudahkan Anda mengelola koleksi buku, anggota, dan peminjaman perpustakaan dengan efisien.<br>
+
+                    🌟 Fitur Unggulan:  
+                    <br>- Kelola Koleksi Buku: Tambah, edit, dan lacak koleksi buku Anda dengan mudah.  
+                    <br>- Manajemen Anggota: Kelola data anggota perpustakaan secara praktis.  
+                    <br>- Sistem Peminjaman & Pengembalia: Otomatisasi proses peminjaman dan pengembalian dengan notifikasi cerdas. <br> 
+
+                    Kami berkomitmen untuk membantu perpustakaan Anda menjadi lebih terorganisir dan produktif. Jika Anda memerlukan bantuan, jangan ragu untuk menghubungi tim dukungan kami.  
+
+                    Selamat menggunakan Library Manager! 📚✨  </p>
+            </div>
+         </div>
+
+        </div>
+    </div>
+</div>
+    <script src="js/bootstrap.js"></script>
+    <script src="js/bootstrap.bundle.js"></script>
+</body>
+</html>
---
+++ koneksi.php
@@ -0,0 +1,10 @@
+<?php
+    $host = "localhost";
+    $username = "root";
+    $password = "";
+    $database = "uts_erixpraditya";
+
+    $koneksi = mysqli_connect($host,$username,$password,$database);
+    
+
+?>
